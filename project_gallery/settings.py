b5964163289a93c0c89dbf8870ffbd47fa979f5e
Mykyta <kosparco@gmail.com>
2025-05-23 17:26:32 +0300

    Update settings.py

--- project_gallery/settings.py
+++ project_gallery/settings.py
@@ -12,10 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "default-fallback")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
